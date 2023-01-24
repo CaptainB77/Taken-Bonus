d7d2cfbbf52add388a1ed0d73ff81369a6e0c479
CaptainB77 <beesquinca@gmail.com>
2023-01-24 06:42:07 -0600

    Load Library

---
+++ TakenBonus.R
@@ -0,0 +1,24 @@
+usethis::edit_git_config()
+usethis::use_git()
+use_github()
+usethis::use_github()
+
+
+library('tidyverse') # essential R packages for data science
+library('data.table')
+
+library('ggplot2') # visualization
+library("ggtext")
+library("highcharter")
+library('gganimate')
+
+library('dplyr') # data manipulation
+library('readr') # input/output
+library('skimr') # overview
+library('tibble') # data wrangling
+library('tidyr') # data wrangling
+library('purrr') # data wrangling
+library('stringr') # string manipulation
+library('forcats') # factor manipulation
+library('janitor') # data cleaning
+library('scales')
